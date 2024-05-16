d8d249872a08a48766114b337af52541d182ad54
Clement <clement.dupic@insa-lyon.fr>
2024-05-17 00:23:15 +0200

    fin du projet anglais

--- PresentationGui.py
+++ PresentationGui.py
@@ -63,6 +63,8 @@ class Gui(ctk.CTk):
         # definition de la police
         self.font = CTkFont(family="Helvetica", size=25)
 
+        self.recette_parcourues = [False, False, False, False]
+
         # Images
         self.image_cookies = CTkImage(PILImage.open("pictures/biscuits.png"), size=(100, 100))
         self.image_chocolate_cake = CTkImage(PILImage.open("pictures/brownie.png"), size=(100, 100))
@@ -81,7 +83,14 @@ class Gui(ctk.CTk):
         self.image_chocolat = CTkImage(PILImage.open("pictures/chocolate-chip.png"), size=(50, 50))
         self.image_star = CTkImage(PILImage.open("pictures/star.png"), size=(50, 50))
         self.image_chocolate_bar = CTkImage(PILImage.open("pictures/chocolate-bar.png"), size=(50, 50))
+        self.image_first = CTkImage(PILImage.open("pictures/first.png"), size=(50, 50))
+        self.image_second = CTkImage(PILImage.open("pictures/second.png"), size=(50, 50))
+        self.image_third = CTkImage(PILImage.open("pictures/third.png"), size=(50, 50))
+
+
+
         self.image_next = CTkImage(PILImage.open("pictures/next.png"), size=(100, 100))
+        self.trophy = CTkImage(PILImage.open("pictures/trophy.png"), size=(100, 100))
 
         self.expectation_cake = CTkImage(PILImage.open("pictures/cake_expectation.png"), size=(500, 500))
         self.expectation_crepe = CTkImage(PILImage.open("pictures/crepes_expectation.png"), size=(500, 500))
@@ -144,6 +153,8 @@ class Gui(ctk.CTk):
         self.frame_selection.rowconfigure(1, weight=1)
         self.frame_selection.rowconfigure(2, weight=1)
         self.frame_selection.rowconfigure(3, weight=1)
+        self.frame_selection.rowconfigure(4, weight=1)
+
 
         #self.image_ruban = CTkImage(PILImage.open("pictures/ruban.png"), size=(1000, 40))
 
@@ -162,11 +173,15 @@ class Gui(ctk.CTk):
         self.button_beignet = CTkButton(self.frame_selection, text="   Doughnut  ", font=self.font, image=self.image_beignet, compound="left", text_color="purple", fg_color="#EEDECF", hover_color="#FED4E7")
         self.button_beignet.grid(row=3, column=0)
 
+        self.button_classement = CTkButton(self.frame_selection, text="", image=self.trophy, fg_color="grey", hover_color="#FED4E7", state="disabled", command= lambda : self.conclusion())
+        self.button_classement.grid(row=4, column=0)
+
+
 
 
         #Frame pour l'affichage des recettes
         self.cooking_recepe_frame = CTkFrame(self.frame_selection, fg_color="#E5B769")
-        self.cooking_recepe_frame.grid(row=0, column=1, rowspan=4, sticky="nsew", pady=20, padx=20)
+        self.cooking_recepe_frame.grid(row=0, column=1, rowspan=5, sticky="nsew", pady=20, padx=20)
 
         self.cooking_recepe_label = CTkLabel(self.cooking_recepe_frame, text="Cooking recepe", font=self.font)
         self.cooking_recepe_label.pack(side="top", pady=20, fill="x")
@@ -180,6 +195,7 @@ class Gui(ctk.CTk):
         self.button_chocolate_cake.bind('<Button-1>', lambda event: self.ecran_recete('chocolate_cake'))
         self.button_crepe.bind('<Button-1>', lambda event: self.ecran_recete('crepe'))
         self.button_beignet.bind('<Button-1>', lambda event: self.ecran_recete('beignet'))
+
 
     def ecran_recete(self, recette: str):
 
@@ -296,7 +312,7 @@ class Gui(ctk.CTk):
             self.label_ingredient_5.pack(side="top", pady=20, fill="x")
             self.update()
             time.sleep(1)
-    
+
             self.label_ingredient_6 = CTkLabel(self.frame_liste_ingredients, text="   20 g of flour", font=self.font, image=self.image_farine, compound="left")
             self.label_ingredient_6.pack(side="top", pady=20, fill="x")
 
@@ -348,7 +364,7 @@ class Gui(ctk.CTk):
 
             self.next_button = CTkButton(self.frame_liste_ingredients, text="  next step", font=self.font)
             self.next_button.pack(side="top", pady=40)
-            self.next_button.bind('<Button-1>', lambda event: self.show_preparation('crepes'))
+            self.next_button.bind('<Button-1>', lambda event: self.show_preparation('crepe'))
 
             self.enable_button()
 
@@ -475,31 +491,98 @@ class Gui(ctk.CTk):
 
         self.cooking_recepe_label.configure(text="Expectation         Reality        ", compound="center", image=self.image_versus)
         if recette== "cookies":
+            self.recette_parcourues[0] = True
             self.label_expectation_cookie = CTkLabel(self.frame_liste_ingredients, text="", image=self.expectation_cookie)
             self.label_expectation_cookie.pack(side="left", pady=50, padx=100, anchor='center')
             self.label_result_cookie = CTkLabel(self.frame_liste_ingredients, text="", image=self.result_cookies)
             self.label_result_cookie.pack(side="left", pady=50, anchor='center')
 
+            value = True
+            for el in self.recette_parcourues:
+                if not el :
+                    value = False
+                    break
+            if value:
+                self.button_classement.configure(fg_color="#E5B769", hover_color="pink", state="normal")
+
+
+
+
         elif recette == "chocolate_cake":
+            self.recette_parcourues[1] = True
+
             self.label_expectation_cake = CTkLabel(self.frame_liste_ingredients, text="", image=self.expectation_cake)
             self.label_expectation_cake.pack(side="left", pady=50, padx=100, anchor='center')
             self.label_result_cake = CTkLabel(self.frame_liste_ingredients, text="", image=self.result_chocolate_cake)
             self.label_result_cake.pack(side="left", pady=50, anchor='center')
 
+            value = True
+            for el in self.recette_parcourues:
+                if not el :
+                    value = False
+                    break
+            if value:
+                self.button_classement.configure(fg_color="#E5B769", hover_color="pink", state="normal")
+
+
         elif recette == "crepe":
+            self.recette_parcourues[2] = True
+
             self.label_expectation_crepe = CTkLabel(self.frame_liste_ingredients, text="", image=self.expectation_crepe)
             self.label_expectation_crepe.pack(side="left", pady=50, padx=100, anchor='center')
             self.label_result_crepe = CTkLabel(self.frame_liste_ingredients, text="", image=self.result_crepe)
             self.label_result_crepe.pack(side="left", pady=50, anchor='center')
+            value = True
+            for el in self.recette_parcourues:
+                if not el :
+                    value = False
+                    break
+            if value:
+                self.button_classement.configure(fg_color="#E5B769", hover_color="pink", state="normal")
+
 
         elif recette == "beignet":
+            self.recette_parcourues[3] = True
+
             self.label_expectation_beignet = CTkLabel(self.frame_liste_ingredients, text="", image=self.expectation_beignet)
             self.label_expectation_beignet.pack(side="left", pady=50, padx=100, anchor='center')
             self.label_result_beignet = CTkLabel(self.frame_liste_ingredients, text="", image=self.result_beignet)
             self.label_result_beignet.pack(side="left", pady=50, anchor='center')
 
-    def conclusion(self):
+            value = True
+            for el in self.recette_parcourues:
+                if not el :
+                    value = False
+                    break
+            if value:
+                self.button_classement.configure(fg_color="#E5B769", hover_color="pink", state="normal")
+
+
+
+    def conclusion(self, event=None):
         ## idée :,mettre un tableau avec une ligne pour chacune des recettes et en colonne : une note/5 pour : facilité, rapidité de consommation,et saveur
+        self.frame_liste_ingredients.destroy()
+        self.frame_liste_ingredients = CTkFrame(self.cooking_recepe_frame, fg_color="#E5B769")
+        self.frame_liste_ingredients.pack(side="top", pady=20, fill="x")
+        self.cooking_recepe_label.configure(text="Ranking", image=None)
+
+
+        self.button1 = CTkButton(self.frame_liste_ingredients, text="", image=self.image_first, compound="left",  fg_color="#E5B769", hover_color="#E5B769", text_color="white")
+        self.button1.pack(side="top", anchor='center', pady=40)
+
+        self.button2 = CTkButton(self.frame_liste_ingredients, text="", image=self.image_second, compound="left",  fg_color="#E5B769", hover_color="#E5B769", text_color="white")
+        self.button2.pack(side="top", anchor='center', pady=40)
+
+        self.button3 = CTkButton(self.frame_liste_ingredients, text="", image=self.image_third, compound="left", fg_color="#E5B769", hover_color="#E5B769", text_color="white")
+        self.button3.pack(side="top", anchor='center', pady=40)
+
+        self.button1.bind('<Button-1>', self.affiche_1)
+        self.button2.bind('<Button-1>', self.affiche_2)
+        self.button3.bind('<Button-1>', self.affiche_3)
+
+
+
+
         self.criteres = ["difficulty", "time", "taste"]
 
         self.cookies_notes = [1, 3, 4]
@@ -508,6 +591,16 @@ class Gui(ctk.CTk):
         self.beignets_notes = [3, 2, 2]
 
         self.sources = ["https://www.marmiton.org/recettes/recette_cookies-maison_86989.aspx", "https://www.marmiton.org/recettes/recette_cake-au-chocolat_17391.aspx", "https://www.marmiton.org/recettes/recette_pate-a-crepes_12372.aspx", "https://cuisine.journaldesfemmes.fr/recette/310213-beignets-rapides-pupperchen"]
+
+    def affiche_1(self, event=None):
+        self.button1.configure(text="     Cookies !", state="disabled", font=self.font)
+
+    def affiche_2(self, event=None):
+        self.button2.configure(text="     Muffin !", state="disabled",  font=self.font)
+
+    def affiche_3(self, event=None):
+        self.button3.configure(text="     Crepes !", state="disabled",  font=self.font)
+
 
     def disable_buttons(self):
         self.button_chocolate_cake.configure(state="disabled")
